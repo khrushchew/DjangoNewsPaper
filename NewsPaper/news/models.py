8dfa61f84681f26787c5267c8001962280a10d67
Никита <popovn2268@gmail.com>
2024-04-25 20:16:55 +0300

    UPD

--- NewsPaper/news/models.py
+++ NewsPaper/news/models.py
@@ -3,16 +3,24 @@ from django.contrib.auth.models import User
 
 
 class Author(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    rating = models.IntegerField(default=0)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='Пользователь')
+    rating = models.IntegerField(default=0, verbose_name='Рейтинг')
     
-    def update_rating(self):
-        post_rating = Post.objects.filter(author=self).aggregate(pr=Coalesce(Sum('rating'), 0))
-        comments_rating = Comment.objects.filter(user=self.user).aggregate(cr=Coalesce(Sum('rating'), 0))
-        posts_comments_rating = Comment.objects.filter(post__author=self).aggregate(pcr=Coalesce(Sum('rating'), 0))
-        self.rating = post_rating * 3 + comments_rating + posts_comments_rating
-        self.save()
-
+    # def update_rating(self):
+    #     post_rating = Post.objects.filter(author=self).aggregate(pr=Coalesce(Sum('rating'), 0))
+    #     comments_rating = Comment.objects.filter(user=self.user).aggregate(cr=Coalesce(Sum('rating'), 0))
+    #     posts_comments_rating = Comment.objects.filter(author=self).aggregate(pcr=Coalesce(Sum('rating'), 0))
+    #     self.rating = post_rating * 3 + comments_rating + posts_comments_rating
+    #     self.save()
+    
+    def __str__(self):
+        return self.user.username
+    
+    class Meta:
+        verbose_name_plural = 'Авторы'
+        verbose_name = 'Автор'
+        ordering = ['rating']
+        
 
 class Category(models.Model):
     category_name = models.CharField(max_length=100, unique=True)
@@ -36,8 +44,16 @@ class Post(models.Model):
     
     title = models.CharField(max_length=100)
     text = models.TextField()
-    rating_of_post = models.IntegerField(default=0)
+    rating = models.IntegerField(default=0)
     
+    def like(self):
+        self.rating += 1
+        self.save()
+    
+    def dislike(self):
+        self.rating -= 1
+        self.save()
+        
     def preview(self):
         return f"{self.text[:124]}..."
 
