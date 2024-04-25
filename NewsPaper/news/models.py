17c391373cd0565fb703e13e271d8e5740accd16
Никита <popovn2268@gmail.com>
2024-04-25 19:34:41 +0300

    First

---
+++ NewsPaper/news/models.py
@@ -0,0 +1,64 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    rating = models.IntegerField(default=0)
+    
+    def update_rating(self):
+        post_rating = Post.objects.filter(author=self).aggregate(pr=Coalesce(Sum('rating'), 0))
+        comments_rating = Comment.objects.filter(user=self.user).aggregate(cr=Coalesce(Sum('rating'), 0))
+        posts_comments_rating = Comment.objects.filter(post__author=self).aggregate(pcr=Coalesce(Sum('rating'), 0))
+        self.rating = post_rating * 3 + comments_rating + posts_comments_rating
+        self.save()
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=100, unique=True)
+
+
+class Post(models.Model):
+    news = 'NE'
+    articles = 'AR'
+    
+    POSITIONS = [
+        (news, "Новость"),
+        (articles, "Статья"),
+    ]
+    
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    
+    type_of_post = models.CharField(max_length=2, choices=POSITIONS, default=news)
+    time_of_creating = models.DateTimeField(auto_now_add=True)
+    
+    category = models.ManyToManyField(Category, through='PostCategory')
+    
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    rating_of_post = models.IntegerField(default=0)
+    
+    def preview(self):
+        return f"{self.text[:124]}..."
+
+
+class PostCategory(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    text = models.CharField(max_length=500)
+    time_of_creating = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
+    
+    def like(self):
+        self.rating += 1
+        self.save()
+        
+    def dislike(self):
+        self.rating -= 1
+        self.save()
